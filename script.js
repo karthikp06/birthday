eaf42434c57786551e978e75b35c5db6a0f05dc6
karthikp06 <karthik.eshwar20@gmail.com>
2026-06-19 15:13:42 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openSurprise() {
-  document.getElementById("surprise").classList.remove("hidden");
-
-  const music = document.getElementById("bgMusic");
-  music.play();
-
-  document.getElementById("surprise").scrollIntoView({ behavior: "smooth" });
-}
